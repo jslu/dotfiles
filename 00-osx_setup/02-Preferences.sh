40e255a6196cd80775ecc7a202e7ab99f8605daa
Ayla-Joe.Lu <joe.lu@aylanetworks.com>
2016-03-28 18:10:02 -0700

    Add additional default settings for screensaver, ctrl-key zooming, trackpad

--- 00-osx_setup/02-Preferences.sh
+++ 00-osx_setup/02-Preferences.sh
@@ -6,24 +6,11 @@ sudo scutil --set HostName Ayla-JoeLu-MBPR
 
 
 #############################################################
-# Set default Finder location to home folder (~/)
-defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
-
-# Disable ext change warning
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
 # Check for software updates daily, not just once per week
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-# Show absolute path in finder's title bar. 
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-
 # Use plain text mode for new TextEdit documents
 defaults write com.apple.TextEdit RichText -int 0
-
-# Make Dock appear on the left (instead of the bottom by default)
-defaults write com.apple.dock orientation -string left && \
 
 # Make bottom-left hotspot turn off screen
 # Hot corners
@@ -40,25 +27,44 @@ defaults write com.apple.dock orientation -string left && \
 # 12: Notification Center
 defaults write com.apple.dock wvous-bl-corner -int 10 && \
 defaults write com.apple.dock wvous-bl-modifier -int 0
-
 # Make hidden app icons translucent in the dock
-defaults write com.apple.Dock showhidden -bool YES
-
+defaults write com.apple.dock showhidden -bool YES
 # Disable automatically re-arrange Spaces based on recent use
 defaults write com.apple.dock mru-spaces -bool false
+# Make Dock appear on the left (instead of the bottom by default)
+defaults write com.apple.dock orientation -string left && \
+# Add a spacer to the left side of the Dock (where the applications are)
+defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
 
 killall Dock
 
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
+# Enable Ctrl(^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 10
+# Set the idle time
+defaults -currentHost write com.apple.screensaver idleTime 60
+
+# Set default Finder location to home folder (~/)
+defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+# Disable ext change warning
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Show absolute path in finder's title bar.
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 # Use current directory as default search scope in Finder
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
 # Show Path bar in Finder
 defaults write com.apple.finder ShowPathbar -bool true
-
 # Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
-
 # Show icons for hard drives, servers, and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true && \
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true && \
@@ -73,6 +79,10 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 #defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true && \
 #defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1 && \
 defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && \
@@ -80,6 +90,9 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && \
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
 
 
 #############################################################
